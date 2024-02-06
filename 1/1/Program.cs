ecc1611d04a69d066990697b1e22e63eed432ead
qwixryl <115480389+OlyaaFed@users.noreply.github.com>
2024-02-06 12:06:04 +0300

    Add files via upload

---
+++ 1/1/Program.cs
@@ -0,0 +1,74 @@
+﻿using _1;
+
+
+
+List<STUDENT> students = new List<STUDENT>();
+
+for (int i = 0;i < 2; i++)
+{
+    STUDENT stud = new STUDENT();
+    stud.InToScreen();
+    students.Add(stud);
+}
+int norma = 3;
+int norma2 = 4;
+
+
+
+foreach (STUDENT stud in students)
+
+    if (stud.Math <= norma || stud.Math <= norma2)
+    {
+        stud.OutToScreen();
+    }
+    else
+    {
+        if (stud.Russkyi <= norma || stud.Russkyi <= norma2)
+        {
+            stud.OutToScreen();
+        }
+        else
+        {
+            if (stud.Fhysic <= norma || stud.Fhysic <= norma2)
+            {
+                stud.OutToScreen();
+            }
+            else
+            {
+                if (stud.Fhysic <= norma || stud.Fhysic <= norma2)
+                {
+                    stud.OutToScreen();
+                }
+                else
+                {
+                    if (stud.Biology <= norma || stud.Biology <= norma2)
+                    {
+                        stud.OutToScreen();
+                    }
+                    else
+                    {
+                        if (stud.Deuthlang <= norma || stud.Deuthlang <= norma2)
+                        {
+                            stud.OutToScreen();
+                        }
+                        if (stud.Deuthlang == 5 && stud.Math == 5 && stud.Russkyi == 5 && stud.Biology == 5 && stud.Fhysic == 5)
+                        {
+                          
+                            Console.WriteLine($"{stud.Nameofstud} - Отличник");
+                        }
+                      
+                    }
+                }
+            }
+        }
+    }
+     
+foreach( STUDENT stud in students)
+    stud.ToString();
+
+
+
+
+
+
+
---
+++ 1/1/STUDENT.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class STUDENT
+    {
+        private int id;
+        private string nameofstud;
+        private string group_name;
+        private int math;
+        private int russkyi;
+        private int fhysic;
+        private int biology;
+        private int deuthlang;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Nameofstud
+        {
+            get { return nameofstud; }
+            set { nameofstud = value; }
+        }
+        public string Groupnameofstud
+        {
+            get { return group_name; }
+            set { group_name = value; }
+        }
+        public int Math
+        {
+            get { return math; }
+            set { math = value; }
+        }
+        public int Russkyi
+        {
+            get { return russkyi; }
+                        set
+            {
+                russkyi = value;
+            }
+        }
+
+        public int Fhysic
+        {
+            get { return fhysic; }
+            set
+            {
+                fhysic = value;
+            }
+        }
+        public int Biology
+        {
+            get { return biology; }
+            set
+            {
+                biology = value;
+            }
+        }
+        public int Deuthlang
+        {
+            get { return deuthlang; }
+            set
+            {
+                deuthlang = value;
+            }
+        }
+
+        public STUDENT()
+        {
+            id = 0;
+            nameofstud = string.Empty;
+            group_name = string.Empty;
+            math = 0;
+            russkyi = 0;
+            fhysic = 0;
+            biology = 0;
+            deuthlang = 0;
+            
+
+        }
+
+        public STUDENT( int id,string nameofstud, string group_name, int math, int russkyi, int fhysic, int biology, int deuthlang)
+        {
+            Id = id;
+            Nameofstud = nameofstud;
+            Groupnameofstud = group_name;
+            Math = math;
+            Russkyi= russkyi;
+            Fhysic= fhysic;
+            Biology= biology;
+            Deuthlang = deuthlang;
+            
+        }
+
+        public void OutToScreen()
+        {
+            Console.WriteLine($"ID:{id}");
+            Console.WriteLine($"ФИО студента: {nameofstud}");
+            Console.WriteLine($"Группа: {group_name}");
+            Console.WriteLine($"Математика: {math}");
+            Console.WriteLine($"Русский:{russkyi}");
+            Console.WriteLine($"Физика: {Fhysic}");
+            Console.WriteLine($"Биология: {biology}");
+            Console.WriteLine($"Немецкий: {Deuthlang}");
+        }
+        public void InToScreen()
+        {
+            Console.WriteLine("ID");
+            Id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("FIO");
+            Nameofstud = Console.ReadLine();
+            Console.WriteLine("Группа");
+            Groupnameofstud = Console.ReadLine();
+            Console.WriteLine("Математика");
+            Math=Convert.ToInt32( Console.ReadLine() );
+            Console.WriteLine("Русский язык");
+            Russkyi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Физика");
+            Fhysic = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Биология");
+            Biology = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Немецкий:");
+            Deuthlang = Convert.ToInt32(Console.ReadLine());
+        }
+        public void SaveToFile(string fileName)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName, true))
+            {
+                writer.WriteLine(this.ToString());
+            }
+        }
+    }
+}
